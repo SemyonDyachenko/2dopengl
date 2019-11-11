913c7427cdff2c2477c960438d3f01aad6342c6d
семён дьяченко <cemchic05@outlook.com>
2019-11-11 14:09:12 +0300

    settings

---
+++ 2dgame/src/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include<GL/glew.h>
+#include <glm/glm.hpp>
+#include <GLFW/glfw3.h>
+#include <time.h>
+
+int main(void)
+{
+	srand(time(0));
+	GLFWwindow* window;
+
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(1440, 900, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		glClear(GL_COLOR_BUFFER_BIT || GL_DEPTH_BUFFER_BIT);
+		glColor3f(149,0,0);
+		glBegin(GL_QUADS);
+		glVertex2f(-0.5f, 0.5f);
+		glVertex2f(0.5f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glVertex2f(-0.5f, -0.5f);
+		glEnd();
+		glTranslatef(0.f, 0.f, -1.f);
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
